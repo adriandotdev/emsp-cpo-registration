3ad43b7417f6a41b553d8da259e7a5e431be93ba
Adrian <marcelonads@gmail.com>
2024-01-26 13:39:03 +0800

    docs: UPDATED README.md

--- database/mysql.js
+++ database/mysql.js
@@ -1,28 +1,31 @@
 require("dotenv").config();
 var mysql = require("mysql2");
 
+// Configuration
+const logger = require("../config/winston");
+
 let pool = mysql.createPool({
 	connectionLimit: process.env.DB_CONNECTION_LIMIT,
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD, // Default and standard password here in sysnet.
+	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 });
 
 pool.getConnection(function (err, connection) {
 	//declaration of db pooling
 	if (err) {
-		return console.error("error: " + err.message);
+		logger.error("error: " + err.message);
 	}
 
 	if (connection) {
-		console.log("Connected to DB");
+		logger.info("Connected to Database");
 		connection.release(); //reuse of connection every after access
 	}
 });
 
 pool.on("release", function (connection) {
-	console.log("Connection %d released", connection.threadId);
+	logger.info("Connection %d released", connection.threadId);
 });
 
 module.exports = pool;
